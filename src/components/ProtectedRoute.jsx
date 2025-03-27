f2484c96819d62f24b6da139bb161623f8912979
Dainius Genzuras <dainiusgenzuras02@gmail.com>
2025-03-27 23:12:41 +0200

    Cleaned ProtectedRoute code

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,26 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
   const navigate = useNavigate();
-  const [shouldRedirect, setShouldRedirect] = useState(false);
 
   useEffect(() => {
     if (!user) {
-      const timer = setTimeout(() => {
-        navigate("/login");
-      }, 5000);
-  
+      const timer = setTimeout(() => navigate("/login"), 5000);
       return () => clearTimeout(timer);
     }
   }, [user, navigate]);
-
-  if (shouldRedirect) {
-    return <Navigate to="/login" />;
-  }
 
   return user ? children : (
     <div className="container mt-4 text-center">
